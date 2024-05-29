16e69a64d31f134a000a2d2d0863059811baa471
Panagiotis Zarogiannis <panzarogiannis@gmail.com>
2024-05-29 20:21:14 +0300

    Close scanner

--- fibonaci.java
+++ fibonaci.java
@@ -5,30 +5,36 @@ public class fibonaci {
         int fibonacci;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give a natural number");
-        fibonacci = scanner.nextInt();
+        try {
 
-        if (fibonacci < 0) {
-            System.out.println("error");
-            return;
-        }
+            System.out.println("Give a natural number");
+            fibonacci = scanner.nextInt();
 
-        int sum = 0;
-        int f0 = 0;
-        int f1 = 1;
+            if (fibonacci < 0) {
+                System.out.println("error");
+                return;
+            }
 
-        System.out.println(f0);
+            int sum = 0;
+            int f0 = 0;
+            int f1 = 1;
 
-        if (fibonacci > 0) {
-            System.out.println(f1);
-        }
+            System.out.println(f0);
 
-        for (int i = 2; i < fibonacci; i++) {
-            sum = f0 + f1;
-            f0 = f1;
-            f1 = sum;
-            System.out.println(sum);
+            if (fibonacci > 0) {
+                System.out.println(f1);
+            }
 
+            for (int i = 2; i < fibonacci; i++) {
+                sum = f0 + f1;
+                f0 = f1;
+                f1 = sum;
+                System.out.println(sum);
+
+            }
+
+        } finally {
+            scanner.close();
         }
     }
 }
